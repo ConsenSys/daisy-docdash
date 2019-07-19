40fb6606a202ffcba72a60a1cfe52938744c269c
Luke Loreti <lhloreti@gmail.com>
2019-07-18 18:09:28 -0700

    Mostly conforming to Daisy styles. Nodemon fixed

--- publish.js
+++ publish.js
@@ -423,7 +423,9 @@ function linktoExternal(longName, name) {
  */
 
 function buildNav(members) {
-    var nav = '<h2><a href="index.html">Home</a></h2>';
+    var logo = '<a href="index.html"><img id="logo" src="https://s3.amazonaws.com/assets.daisypayments.com/white_logo.svg" alt="Home"/></a>';
+    var search = '<input type="text" id="nav-search" placeholder="Search" />';
+    var nav = '<header>' + logo + 'Documentation v1</header><section>' + search;
     var seen = {};
     var seenTutorials = {};
     var docdash = env && env.conf && env.conf.docdash || {};
@@ -471,7 +473,7 @@ function buildNav(members) {
             nav += '<h3>Global</h3><ul>' + globalNav + '</ul>';
         }
     }
-
+    nav += '</section>'
     return nav;
 }
 
