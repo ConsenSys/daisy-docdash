2a58bc46b0cdd677d044a33347906fbeb79eb33f
Luke Loreti <lhloreti@gmail.com>
2019-08-05 16:27:22 -0400

    Docs generated with jsdoc.conf.js file. gh-pages added. Tutorials only grabbed from daisy-widget/docs; script required to put all user guides in one tutorials/ dir in the future

---
+++ jsdoc.conf.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = {
+  plugins: ["plugins/markdown"],
+  readme: "README",
+  sourceType: "module",
+
+  source: {
+    include: [
+      "../daisy-sdk/README.md", 
+      "../daisy-sdk/browser", 
+      "../daisy-sdk/common", 
+      "../daisy-sdk/private"
+    ],
+  },
+  opts: {
+    template: "./",
+    encoding: "utf8",
+    destination: "dist/",
+    "tutorials": "../daisy-widget/docs/tutorials",
+    recurse: true,
+    verbose: true,
+  },
+  templates: {
+    default: {
+      staticFiles: {
+        include: [
+            "../daisy-widget/docs/tutorials/assets"
+        ],
+      }
+    }
+  },
+  docdash: {
+    sectionOrder: [
+      "Tutorials",
+      "Classes",
+      "Modules",
+      "Externals",
+      "Events",
+      "Namespaces",
+      "Mixins",
+      "Interfaces"
+    ]
+  },
+};
